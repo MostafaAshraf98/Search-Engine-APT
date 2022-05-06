fa6ea0c869c024c9f306174c764e7b798f7a97a1
ahmedmoh123 <58375227+ahmedmoh1234@users.noreply.github.com>
2022-05-06 03:26:34 +0200

    updated html reading

--- indexer.java
+++ indexer.java
@@ -12,31 +12,39 @@ public class indexer
 {
     static Map<String,ArrayList<ArrayList<String>>> index = new HashMap<String,ArrayList<ArrayList<String>>>();
 
+    static String webpagesPath = "./webPages/";
     public static void main(String[] args) throws IOException
     {
-        String filename = "index.html";
-        Document doc = readHTMLFile(filename);
-        Element body = doc.select("body").first();
-        Element head = doc.select("head").first();
-        if (!(head.hasText()))
+        var docs = readAllHTML();
+        for (var doc : docs)
         {
-            System.out.println("No head tag found");
-            head = null;
-        }
-        
-        if (!(body.hasText()))
-        {
-            System.out.println("No body tag found");
-            body = null;
+            //Apply logic here and add to database
         }
 
     }
 
+    private static ArrayList<Document> readAllHTML() throws IOException
+    {
+        ArrayList<Document> docs = new ArrayList<Document>();
+        File folder = new File(webpagesPath);
+        File[] listOfFiles = folder.listFiles();
+        System.out.println("Number of files: " + listOfFiles.length);
+        for (File file : listOfFiles)
+        {
+            if (file.isFile())
+            {
+                System.out.println(file.getName());
+                Document doc = readHTMLFile(webpagesPath +file.getName());
+                docs.add(doc);
+            }
+        }
+        return docs;
+    }
+
     private static Document readHTMLFile(String fileName) throws IOException
     {
-        File htmlFile = new File("./" + fileName);
+        File htmlFile = new File(fileName);
         Document doc = Jsoup.parse(htmlFile, "UTF-8");
-        //System.out.println(doc);
         return doc;
     }
     
