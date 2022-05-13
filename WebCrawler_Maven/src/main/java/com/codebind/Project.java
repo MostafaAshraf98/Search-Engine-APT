ab7a29b80f27042ddea6e4fed1b8f6206755a216
SarahOssama <62894826+SarahOssama@users.noreply.github.com>
2022-05-14 01:05:13 +0200

    PageRank not integrated

--- WebCrawler_Maven/src/main/java/com/codebind/Project.java
+++ WebCrawler_Maven/src/main/java/com/codebind/Project.java
@@ -8,17 +8,18 @@ import com.mongodb.client.MongoDatabase;
 import java.io.IOException;
 
 import com.codebind.indexerPack.Indexer;
+import com.codebind.rankerPack.PageRank;
+import com.codebind.rankerPack.SearchResult;
 
 public class Project {
     public static void main(String[] args) throws IOException {
-    	MongoClient client = MongoClients.create(
-                "mongodb+srv://Mostafa_98:mostafa123@webcrawler.6mfpo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
-        // Getting the dataBase from this client.
-        MongoDatabase db = client.getDatabase("WebCrawler");
-        WebCrawler webcrawler = new WebCrawler();
-        WebCrawler.Web(args,db);
+        // MongoClient client = MongoClients.create(
+        // "mongodb+srv://Mostafa_98:mostafa123@webcrawler.6mfpo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+        // // Getting the dataBase from this client.
+        // MongoDatabase db = client.getDatabase("WebCrawler");
+        // WebCrawler webcrawler = new WebCrawler();
+        // WebCrawler.Web(args,db);
 
-        Indexer ind = new Indexer();
-        ind.indexer(args,db);
+        PageRank.rank();
     }
 }
---
+++ WebCrawler_Maven/src/main/java/com/codebind/rankerPack/SearchResult.java
@@ -0,0 +1,20 @@
+package com.codebind.rankerPack;
+
+public class SearchResult {
+    public static WebPage[] searchResults;
+
+    public SearchResult() {
+        WebPage P0 = new WebPage(0, new String[] { "2" }, 0, 0.25, 2, new Integer[] { 1, 2 });
+        WebPage P1 = new WebPage(1, new String[] { "0", "2" }, 0, 0.25, 1, new Integer[] { 3 });
+        WebPage P2 = new WebPage(2, new String[] { "0", "3" }, 0, 0.25, 3, new Integer[] { 0, 1, 3 });
+        WebPage P3 = new WebPage(3, new String[] { "1", "2" }, 0, 0.25, 1, new Integer[] { 2 });
+
+        searchResults = new WebPage[4];
+        searchResults[0] = P0;
+        searchResults[1] = P1;
+        searchResults[2] = P2;
+        searchResults[3] = P3;
+
+    }
+
+}
