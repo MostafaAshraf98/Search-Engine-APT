92b4756bc5ff7ebd7c246a8a36f0b6cdedfb3de6
nadaabdelgayed <nada.ahmed15@stemmaadi.edu.eg>
2022-05-16 17:50:12 +0200

    fixed stemming function

--- WebCrawler_Maven/src/main/java/com/codebind/indexerPack/Indexer.java
+++ WebCrawler_Maven/src/main/java/com/codebind/indexerPack/Indexer.java
@@ -76,98 +76,96 @@ public class Indexer {
         	 result=true;
     	 return result;
     }
-    public static void stemWord(String word) {
+    public static String stemWord(String word) {
     	PorterStemmer stemmer = new PorterStemmer();
-    	
-//    	stem.setCurrent((char*)word);
-    	System.out.println(stemmer.stem(word));
-//    	String result = stem.getCurrent();
+    
+    	return stemmer.stem(word);
     }
-//    private static void index(MongoDatabase db) throws IOException {
-//        ArrayList<Document> docs = readAllHTML(db);
-//        for (Document doc : docs) {
-//            // Get all tags in a document
-//            Elements all = doc.getAllElements();
-//            for (Element e : all) {
-//                // For each tag, get the text
-//                String text = e.text();
-//                String[] words = text.split(" ");
-//                for (String word : words) {
-//                    word = word.toLowerCase();
-//                    // Replacing all non-alphanumeric (not from "[^a-zA-Z0-9]") characters with
-//                    // empty string
-//                    word = word.replaceAll("[^a-zA-Z0-9]", "");
-//                    // TODO: remove stop words
-//                    boolean isStopWord = isStopword(word);
-//                    // TODO: stemming
-//                    // TODO: remove words with length < 3
-//
-//                    // If the word is not empty, add it to inverted file
-//                    if (word.length() > 0) {
-//                        if (invertedFile.containsKey(word)) {
-//                            // If the word is already in the inverted file
-//
-//                            // doc.baseUri() returns the url of the document
-//                            // ex. C:\Users\dusername\Desktop\Java\.\125241216.html
-//                            // get the last part of the url
-//                            // ex. 125241216.html
-//                            String fileName = doc.baseUri().substring(doc.baseUri().lastIndexOf("\\") + 1);
-//
-//                            if (invertedFile.get(word).containsKey(fileName)) {
-//                                // If the documnet is already in the inverted file
-//                                Pair<Integer, Map<String, Integer>> pair = invertedFile.get(word).get(fileName);
-//
-//                                // Update the term frequency
-//                                int newTermFreq = pair.getValue0() + 1;
-//
-//                                // Check if the position of the word in the document is already in the inverted
-//                                // file
-//                                if (pair.getValue1().containsKey(e.tagName())) {
-//                                    // If the position is already in the inverted file, update the term frequency
-//                                    int pos = pair.getValue1().get(e.tagName());
-//                                    pos++;
-//                                    Map<String, Integer> newMap = pair.getValue1();
-//                                    newMap.put(e.tagName(), pos);
-//                                    pair = new Pair<Integer, Map<String, Integer>>(newTermFreq, newMap);
-//                                    invertedFile.get(word).put(fileName, pair);
-//                                } else {
-//                                    // If the position is not in the inverted file, add it
-//                                    Map<String, Integer> newMap = pair.getValue1();
-//                                    newMap.put(e.tagName(), 1);
-//                                    pair = new Pair<Integer, Map<String, Integer>>(newTermFreq, newMap);
-//                                    invertedFile.get(word).put(fileName, pair);
-//
-//                                }
-//                            } else {
-//                                // If the documnet is not in the inverted file
-//                                invertedFile.get(word).put(fileName,
-//                                        new Pair<Integer, Map<String, Integer>>(1, new HashMap<String, Integer>() {
-//                                            {
-//                                                put(e.tagName(), 1);
-//                                            }
-//                                        }));
-//                            }
-//
-//                        } else {
-//                            // If the word is not in the inverted file
-//
-//                            String fileName = doc.baseUri().substring(doc.baseUri().lastIndexOf("\\") + 1);
-//                            invertedFile.put(word, new HashMap<String, Pair<Integer, Map<String, Integer>>>() {
-//                                {
-//                                    put(fileName,
-//                                            new Pair<Integer, Map<String, Integer>>(1, new HashMap<String, Integer>() {
-//                                                {
-//                                                    put(e.tagName(), 1);
-//                                                }
-//                                            }));
-//                                }
-//                            });
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//    }
+    private static void index(MongoDatabase db) throws IOException {
+        ArrayList<Document> docs = readAllHTML(db);
+        for (Document doc : docs) {
+            // Get all tags in a document
+            Elements all = doc.getAllElements();
+            for (Element e : all) {
+                // For each tag, get the text
+                String text = e.text();
+                String[] words = text.split(" ");
+                for (String word : words) {
+                    word = word.toLowerCase();
+                    // Replacing all non-alphanumeric (not from "[^a-zA-Z0-9]") characters with
+                    // empty string
+                    word = word.replaceAll("[^a-zA-Z0-9]", "");
+                    // TODO: remove stop words
+                    boolean isStopWord = isStopword(word);
+                    // TODO: stemming
+                    // TODO: remove words with length < 3
+
+                    // If the word is not empty, add it to inverted file
+                    if (word.length() > 0) {
+                        if (invertedFile.containsKey(word)) {
+                            // If the word is already in the inverted file
+
+                            // doc.baseUri() returns the url of the document
+                            // ex. C:\Users\dusername\Desktop\Java\.\125241216.html
+                            // get the last part of the url
+                            // ex. 125241216.html
+                            String fileName = doc.baseUri().substring(doc.baseUri().lastIndexOf("\\") + 1);
+
+                            if (invertedFile.get(word).containsKey(fileName)) {
+                                // If the documnet is already in the inverted file
+                                Pair<Integer, Map<String, Integer>> pair = invertedFile.get(word).get(fileName);
+
+                                // Update the term frequency
+                                int newTermFreq = pair.getValue0() + 1;
+
+                                // Check if the position of the word in the document is already in the inverted
+                                // file
+                                if (pair.getValue1().containsKey(e.tagName())) {
+                                    // If the position is already in the inverted file, update the term frequency
+                                    int pos = pair.getValue1().get(e.tagName());
+                                    pos++;
+                                    Map<String, Integer> newMap = pair.getValue1();
+                                    newMap.put(e.tagName(), pos);
+                                    pair = new Pair<Integer, Map<String, Integer>>(newTermFreq, newMap);
+                                    invertedFile.get(word).put(fileName, pair);
+                                } else {
+                                    // If the position is not in the inverted file, add it
+                                    Map<String, Integer> newMap = pair.getValue1();
+                                    newMap.put(e.tagName(), 1);
+                                    pair = new Pair<Integer, Map<String, Integer>>(newTermFreq, newMap);
+                                    invertedFile.get(word).put(fileName, pair);
+
+                                }
+                            } else {
+                                // If the documnet is not in the inverted file
+                                invertedFile.get(word).put(fileName,
+                                        new Pair<Integer, Map<String, Integer>>(1, new HashMap<String, Integer>() {
+                                            {
+                                                put(e.tagName(), 1);
+                                            }
+                                        }));
+                            }
+
+                        } else {
+                            // If the word is not in the inverted file
+
+                            String fileName = doc.baseUri().substring(doc.baseUri().lastIndexOf("\\") + 1);
+                            invertedFile.put(word, new HashMap<String, Pair<Integer, Map<String, Integer>>>() {
+                                {
+                                    put(fileName,
+                                            new Pair<Integer, Map<String, Integer>>(1, new HashMap<String, Integer>() {
+                                                {
+                                                    put(e.tagName(), 1);
+                                                }
+                                            }));
+                                }
+                            });
+                        }
+                    }
+                }
+            }
+        }
+    }
 
     private static ArrayList<Document> readAllHTML(MongoDatabase db) throws IOException {
         ArrayList<Document> docs = new ArrayList<Document>();
